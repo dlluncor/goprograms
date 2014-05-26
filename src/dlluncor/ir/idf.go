4bef2df1ea06e6b2c685381cbd7df70ba0f9c374
dlluncor <david.lluncor@gmail.com>
2014-05-26 09:12:54 -0700

    ~serialize objects and write them.

--- src/dlluncor/ir/idf.go
+++ src/dlluncor/ir/idf.go
@@ -3,7 +3,11 @@
 package ir
 
 import (
+        "bytes"
+        "encoding/gob"
 	"fmt"
+        "os"
+        "log"
 	"reflect"
 	"sort"
 
@@ -25,7 +29,12 @@ func (s sortT) Len() int           { return len(s) }
 func (s sortT) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
 func (s sortT) Less(i, j int) bool { return s[i].Num < s[j].Num }
 
-func (i *indCounter) Count() {
+// intermediate to write to disk
+type indexInfo struct {
+ TF map[mr.Key]types.TF
+}
+
+func (i *indCounter) Count() *indexInfo {
 	// Map doc to words within doc.
 	spec := &mr.Spec{
 		Input:   docToInterface(i.docs),
@@ -42,6 +51,7 @@ func (i *indCounter) Count() {
 		fmt.Printf("\n")
 	}
 
+        var outTF = make(map[mr.Key]types.TF)
 	{
 		// Reduce word counts to idf scores.
 		spec := &mr.Spec{
@@ -50,9 +60,9 @@ func (i *indCounter) Count() {
 			Reducer: &mappers.TermReducer{},
 			Output:  mr.Output{"map", tfType},
 		}
-		out := (mr.Run(spec).Interface()).(map[mr.Key]types.TF)
+		outTF = (mr.Run(spec).Interface()).(map[mr.Key]types.TF)
 		ts := []*types.TF{}
-		for t, tf := range out {
+		for t, tf := range outTF {
 			newT := &types.TF{
 				Term: string(t),
 				Num:  tf.Num,
@@ -65,12 +75,41 @@ func (i *indCounter) Count() {
 			fmt.Printf("%v\n", t)
 		}
 	}
+  return &indexInfo{
+    TF: outTF,
+  }
+}
 
+func check(err error) {
+  if err != nil {
+    log.Fatalf("%v\n", err)
+  }
+}
+
+// Write writes out the index to disk.
+func (i *indCounter) Write(inf *indexInfo) {
+  var network bytes.Buffer
+  enc := gob.NewEncoder(&network)
+
+  // Term metadata for QRewrite.
+  check(enc.Encode(inf))
+  f, err := os.Create("qReWrite.dat")
+  check(err)
+  defer f.Close()
+  f.Write(network.Bytes())
+
+  
+  var in indexInfo
+  dec := gob.NewDecoder(&network)
+  check(dec.Decode(&in))
+  fmt.Println("***------------******") 
+  fmt.Printf("%v\n", in) 
 }
 
 func BuildIndex() {
 	c := &indCounter{
 		docs: allDocs,
 	}
-	c.Count()
+	inf := c.Count()
+        c.Write(inf)
 }
